f12e7a635e6626e716709d5bcb2321d996f61bd4
snozza <andrewcsnead@gmail.com>
2016-12-18 19:00:29 +0000

    add error middleware to server

--- server/index.js
+++ server/index.js
@@ -3,10 +3,20 @@ const path = require('path');
 
 const app = new Express();
 
+app.use(`/static`, Express.static(path.join(__dirname, '../client/build/static')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(`/static`, Express.static(path.join(__dirname, '../client/build/static')));
+// Error middleware
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
 
 module.exports = app;
