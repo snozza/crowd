ef30d95f4174945700a33eed29195655436b7817
snozza <andrewcsnead@gmail.com>
2016-12-18 21:31:00 +0000

    update static dir

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = new Express();
 
-app.use(`/static`, Express.static(path.join(__dirname, '../client/build')));
+app.use(`/static`, Express.static(path.join(__dirname, '../client/build/static')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Error middleware
